1e0c7717043dbbe4076367483246a7434048789c
dobrovolskiy <fro3zn@mail.ru>
2023-01-30 21:33:12 +0300

    Added new tests

---
+++ pg_stage/tests/sql/test_parse_copy_values_with_mutation.sql
@@ -0,0 +1,20 @@
+COMMENT ON COLUMN table_1.perishable_token IS 'anon: {"mutation_name": "empty_string"}';
+
+COPY table_1 (id, crypted_password, persistence_token, created_at, updated_at, active, first_name, last_name, agency_id, phone, perishable_token, extension, commissioned, region, division, location, last_request_at) FROM stdin;
+07750c56-fb37-46e1-b7b6-da530704c056	400$8$4c$c11df6facaefc6bc$93a657fb3c6e4cd1fd3255d3bd1edd18ffc4a8092e2616b9e5ca7954b2c52504	86a97ff982e87ed5af7d90ab2ce31d4e89a3af3e6a0490b067bb8213aea7a4ee0eeafae1d8fe3c6f990aead095092fcf852004b18e484ef22569aebf64c3747f	2016-06-03 18:23:06.25685	2016-06-03 18:23:06.25685	t	C.J.	Cregg	\N	1231231234	hw1rFSX7yJBz65lDVzYi	\N	f	\N	\N	\N	\N
+d489bda7-3212-4026-aef9-879d3242ad0c	400$8$50$5555852e67bf920f$161b41afd99159609151f22fbc4efdccb925244606a87fc635954a71e8f7606f	630aaf10f3edcb396181535476bccfd369e1ee9281cbc218d0861e6117b9839219cb403abed3b59468b2564c229d78b0cb610d0e16fa4a03dc4d93ed3d55deb3	2016-06-03 18:23:06.417694	2016-06-03 18:23:06.417694	t	Leo	McGarry	\N	1231231234	ojbfkdLiQOOHvqxuTsg	\N	f	\N	\N	\N	\N
+ab344f75-d78a-47ba-a4f7-121275b3b241	400$8$4a$fb1a833f95079502$8b6ddd327381860e18bce53ee1a3249d5237de4f4344c06ad940ba8cfc6a2259	06fa5d0f3a4d62ff60717ff3315301129a5bcd152f63f073b67d2e6a835b098524b1d097367283c97ca75db9154cf61d6a918248eb68eaf8cd83d1a87cdd92dc	2016-06-03 18:23:06.563866	2016-06-03 18:23:06.563866	t	Donna	Moss	\N	1231231234	p8MikeuBbbbFZ2i7V1gT	\N	f	\N	\N	\N	\N
+f7bd4a7f-b63f-41c3-83bd-9683f33bb4b3	400$8$50$8368e20d878ef1b1$b271e3af024b424b7256592439a35d6eb224b4dc769ac9f2fa564b282efb8a47	02cbd8d1e9690c7786375f9f6ec4da6fa3c2ead70d41a7aa1d7fe631477b42635210f0e8fa7579f6f4b5afdb364d9465623a2861ecff75fa3a052c9b2a9fc2dd	2016-06-03 18:23:06.739671	2016-06-03 18:23:06.739671	t	Charlie	Young	\N	1231231234	rkU7yVplV5FMsoJFDkaM	\N	f	\N	\N	\N	\N
+abe9fd39-f0b3-4ec6-9006-9783fbdade07	400$8$50$ef1eb813f8d8b99d$2932a9dc75d10f13525e43c29726061d99b4ebdff2e9f9951eb6e2393f36d221	b09909b6e83938dd41ffb5e931eeb3d646b1856dfb74a81acb1697b6d8466468047fe92286e011a4634c71b8d8775c7d5a31e19ce111bd31d0a61a4faf93d6af	2016-06-03 18:23:05.797143	2016-06-07 21:53:45.149755	t	Fun	Buyer	5030cc27-e7a8-4768-b355-20cdcf9efc53	1112223333	8npwjv7m8NmCTT9eZdIK	\N	f	\N	\N	\N	2016-06-07 21:53:45.148823
+\.
+
+COMMENT ON COLUMN schema.table_1.email IS 'anon: {"mutation_name": "email"}';
+COMMENT ON COLUMN schema.table_1.password_salt IS 'anon: {"mutation_name": "null"}';
+
+COPY schema.table_2 (id, email, crypted_password, password_salt, created_at, updated_at, active, first_name, last_name, agency_id, phone, perishable_token, extension, commissioned, region, division, location, last_request_at) FROM stdin;
+07750c56-fb37-46e1-b7b6-da530704c056	cj@example.com	400$8$4c$c11df6facaefc6bc$93a657fb3c6e4cd1fd3255d3bd1edd18ffc4a8092e2616b9e5ca7954b2c52504	v8BMktHnOeokEBTy6As	2016-06-03 18:23:06.25685	2016-06-03 18:23:06.25685	t	C.J.	Cregg	\N	1231231234	hw1rFSX7yJBz65lDVzYi	\N	f	\N	\N	\N	\N
+d489bda7-3212-4026-aef9-879d3242ad0c	leo@example.com	400$8$50$5555852e67bf920f$161b41afd99159609151f22fbc4efdccb925244606a87fc635954a71e8f7606f	c5d6v3NS97D3wYkUltFQ	2016-06-03 18:23:06.417694	2016-06-03 18:23:06.417694	t	Leo	McGarry	\N	1231231234	ojbfkdLiQOOHvqxuTsg	\N	f	\N	\N	\N	\N
+ab344f75-d78a-47ba-a4f7-121275b3b241	donna@example.com	400$8$4a$fb1a833f95079502$8b6ddd327381860e18bce53ee1a3249d5237de4f4344c06ad940ba8cfc6a2259	2IcBeSh6IVVCJyZpqBt	2016-06-03 18:23:06.563866	2016-06-03 18:23:06.563866	t	Donna	Moss	\N	1231231234	p8MikeuBbbbFZ2i7V1gT	\N	f	\N	\N	\N	\N
+f7bd4a7f-b63f-41c3-83bd-9683f33bb4b3	charlie@example.com	400$8$50$8368e20d878ef1b1$b271e3af024b424b7256592439a35d6eb224b4dc769ac9f2fa564b282efb8a47	QOqWQ24CMiRNuPUz8f5	2016-06-03 18:23:06.739671	2016-06-03 18:23:06.739671	t	Charlie	Young	\N	1231231234	rkU7yVplV5FMsoJFDkaM	\N	f	\N	\N	\N	\N
+abe9fd39-f0b3-4ec6-9006-9783fbdade07	fun@example.com	400$8$50$ef1eb813f8d8b99d$2932a9dc75d10f13525e43c29726061d99b4ebdff2e9f9951eb6e2393f36d221	uMYA6c3A7uGoI0aEbJ8R	2016-06-03 18:23:05.797143	2016-06-07 21:53:45.149755	t	Fun	Buyer	5030cc27-e7a8-4768-b355-20cdcf9efc53	1112223333	8npwjv7m8NmCTT9eZdIK	\N	f	\N	\N	\N	2016-06-07 21:53:45.148823
+\.
--- pg_stage/tests/test_prepared_data.py
+++ pg_stage/tests/test_prepared_data.py
@@ -10,7 +10,7 @@ def obfuscator_object():
 
 def test_parse_copy_values_with_delete_table(obfuscator_object: Obfuscator):
     """
-    Arrange: Дамп таблицы, данные которой необходимо удалить
+    Arrange: Дамп таблиц, данные которых необходимо удалить
     Act: Вызов функции `_parse_line` класса Obfuscator
     Assert: В stdout нет данных из таблицы
     """
@@ -33,3 +33,47 @@ def test_parse_copy_values_with_delete_table(obfuscator_object: Obfuscator):
         '\\.',
     ]
     assert assert_result == result
+
+
+def test_parse_copy_values_with_mutation(obfuscator_object: Obfuscator):
+    """
+    Arrange: Дамп таблиц, в которых необходимо мутировать одно или несколько полей
+    Act: Вызов функции `_parse_line` класса Obfuscator
+    Assert: В stdout данные полей мутированы
+    """
+    with open('pg_stage/tests/sql/test_parse_copy_values_with_mutation.sql') as file:
+        dump_sql = file.read()
+
+    result = set()
+    for line in dump_sql.splitlines():
+        new_line = obfuscator_object._parse_line(line=line)
+        if new_line is not None:
+            result.add(new_line)
+
+    assert not (
+        {'cj@example.com', 'leo@example.com', 'donna@example.com', 'charlie@example.com', 'fun@example.com'}
+        & result
+    )
+    assert not (
+        {
+            '86a97ff982e87ed5af7d90ab2ce31d4e89a3af3e6a0490b067bb8213aea7a4ee0eeafae1d8fe3c6f990aead095092fcf852004b18'
+            'e484ef22569aebf64c3747f',
+            '630aaf10f3edcb396181535476bccfd369e1ee9281cbc218d0861e6117b9839219cb403abed3b59468b2564c229d78b0cb610d0e1'
+            '6fa4a03dc4d93ed3d55deb3',
+            '06fa5d0f3a4d62ff60717ff3315301129a5bcd152f63f073b67d2e6a835b098524b1d097367283c97ca75db9154cf61d6a918248e'
+            'b68eaf8cd83d1a87cdd92dc',
+            '02cbd8d1e9690c7786375f9f6ec4da6fa3c2ead70d41a7aa1d7fe631477b42635210f0e8fa7579f6f4b5afdb364d9465623a2861e'
+            'cff75fa3a052c9b2a9fc2dd',
+            'b09909b6e83938dd41ffb5e931eeb3d646b1856dfb74a81acb1697b6d8466468047fe92286e011a4634c71b8d8775c7d5a31e19ce'
+            '111bd31d0a61a4faf93d6af',
+        } & result
+    )
+    assert not (
+        {
+            'v8BMktHnOeokEBTy6As',
+            'c5d6v3NS97D3wYkUltFQ',
+            '2IcBeSh6IVVCJyZpqBt',
+            'QOqWQ24CMiRNuPUz8f5',
+            'uMYA6c3A7uGoI0aEbJ8R',
+        } & result
+    )
