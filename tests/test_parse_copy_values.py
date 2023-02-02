9c87b0cf2bf761b0dcb9256cbfcb9902508da807
dobrovolskiy <fro3zn@mail.ru>
2023-02-02 20:11:16 +0300

    Added new test

--- tests/test_parse_copy_values.py
+++ tests/test_parse_copy_values.py
@@ -5,7 +5,7 @@ from src.pg_stage.obfuscator import Obfuscator
 
 @pytest.fixture(autouse=True)
 def obfuscator_object_with_delete_tables_by_pattern():
-    return Obfuscator(delete_tables_by_pattern=[r'table', r'schema\.table'])
+    return Obfuscator(delete_tables_by_pattern=[r'_table', r'schema\.\_table'])
 
 
 @pytest.mark.parametrize("table_name", ['table', 'schema.table'])
@@ -37,8 +37,8 @@ def test_parse_copy_values_without_delete_comment(obfuscator_object: Obfuscator,
     assert obfuscator_object._is_data  # nosec
 
 
-@pytest.mark.parametrize("table_name", ['table', 'schema.table'])
-def test_parse_copy_values_without_delete_comment(
+@pytest.mark.parametrize("table_name", ['_table', 'schema._table'])
+def test_parse_copy_values_without_delete_tables_by_pattern(
     obfuscator_object_with_delete_tables_by_pattern: Obfuscator,
     table_name: str,
 ):
@@ -52,4 +52,23 @@ def test_parse_copy_values_without_delete_comment(
         line=f'COPY {table_name} (column_1, column_2) FROM stdin;',
     )
     assert obfuscator_object_with_delete_tables_by_pattern._is_delete  # nosec
+    assert obfuscator_object_with_delete_tables_by_pattern._is_data  # nosec
+
+
+@pytest.mark.parametrize("table_name", ['table', 'schema.table'])
+def test_parse_copy_values_without_delete_tables_by_pattern_and_not_found_table(
+    obfuscator_object_with_delete_tables_by_pattern: Obfuscator,
+    table_name: str,
+):
+    """
+    Arrange: Строка копирования данных в таблицу, которая не должна быть удалена, т.к. не входит в список
+    delete_tables_by_pattern
+    delete_tables_by_pattern
+    Act: Вызов функции `_parse_line` класса Obfuscator
+    Assert: Флаг _is_delete не активен
+    """
+    obfuscator_object_with_delete_tables_by_pattern._parse_line(
+        line=f'COPY {table_name} (column_1, column_2) FROM stdin;',
+    )
+    assert not obfuscator_object_with_delete_tables_by_pattern._is_delete  # nosec
     assert obfuscator_object_with_delete_tables_by_pattern._is_data  # nosec
