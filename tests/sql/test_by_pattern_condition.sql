337f4c37c1be506b72fd856a833f751edb64760d
dobrovolskiy <fro3zn@mail.ru>
2023-02-25 20:41:34 +0300

    Added conditions with tests

---
+++ tests/sql/test_by_pattern_condition.sql
@@ -0,0 +1,5 @@
+COMMENT ON COLUMN table_1.email IS 'anon: {"mutation_name": "email", "conditions": [{"column_name": "email", "operation": "by_pattern", "value": "\\w+\\@\\w+\\.\\w{2}"}]}';
+COPY table_1 (id, email, active) FROM stdin;
+1	test@mail.ru	t
+2	test@mail.ru	f
+\.
---
+++ tests/sql/test_equal_condition.sql
@@ -0,0 +1,5 @@
+COMMENT ON COLUMN table_1.email IS 'anon: {"mutation_name": "email", "conditions": [{"column_name": "active", "operation": "equal", "value": "t"}]}';
+COPY table_1 (id, email, active) FROM stdin;
+1	test@mail.ru	t
+2	test@mail.ru	f
+\.
